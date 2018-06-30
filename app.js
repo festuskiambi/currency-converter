af71f8fbe79606f6efc75dcbf62e1e5e1bb5074d
Festus kiambi <kiamfestoh@gmail.com>
2018-06-30 22:40:16 +0300

    fetch and convert currency from api

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 /*jshint esversion: 6 */
 const currencySelector = document.getElementsByTagName("select");
-const https = require("https");
-
 
 window.addEventListener("load", e => {
   getCurrencies();
+  document.getElementById("from_amount").value = "1";
 });
 
 async function getCurrencies() {
@@ -22,44 +21,40 @@ async function getCurrencies() {
     currencySelector[0].appendChild(option.cloneNode(true));
     currencySelector[1].appendChild(option);
   }
+  getSelectedValues();
 }
 
+const getSelectedValues = () => {
+  const fromSelect = document.getElementsByTagName("select")[0];
+  const toSelect = document.getElementsByTagName("select")[1];
+  const fromSelectedCurrency = fromSelect.options[fromSelect.selectedIndex].id;
+  const toSelectedCurrency = toSelect.options[toSelect.selectedIndex].id;
+  const amount = document.getElementById("from_amount").value;
+  convertCurrency(amount, fromSelectedCurrency, toSelectedCurrency);
+};
 
-function convertCurrency(amount, fromCurrency, toCurrency, cb) {
-
+const convertCurrency = (amount, fromCurrency, toCurrency) => {
   fromCurrency = encodeURIComponent(fromCurrency);
   toCurrency = encodeURIComponent(toCurrency);
   const query = `${fromCurrency}_${toCurrency}`;
+  const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
 
-  const url = `https://www.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+  return fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(conrversion_rate => {
+      const conrversion_rate_value = conrversion_rate[query];
+      if (conrversion_rate_value) {
+        const total = conrversion_rate_value * amount;
+        const convertedAmount = Math.round(total * 100) / 100;
+        console.log(convertedAmount);
+        document.getElementById("target_amount").innerHTML = convertedAmount;
 
-  https.get(url, res => {
-    let body = '';
-
-    res.on('data', chunk => {
-      body += chunk;
-    });
-
-    res.on('end', () => {
-      try {
-        const jsonObj = JSON.parse(body);
-
-        const val = jsonObj[query];
-        if (val) {
-          const total = val * amount;
-          cb(null, Math.round(total * 100) / 100);
-        } else {
-          const err = new Error(`Value not found for ${query}`);
-          console.log(err);
-          cb(err);
-        }
-      } catch (e) {
-        console.log("Parse error: ", e);
-        cb(e);
+      } else {
+        const err = new Error(`Value not found for ${query}`);
+        console.log(err);
+        cb(err);
       }
     });
-  }).on('error', e => {
-    console.log("Got an error: ", e);
-    cb(e);
-  });
-}
+};
