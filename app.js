1c3fbaf572fdfafa1217e4f41e30ef763b8e4bf2
Festus kiambi <kiamfestoh@gmail.com>
2018-06-30 10:10:36 +0300

    add function to convert currency from api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /*jshint esversion: 6 */
 const currencySelector = document.getElementsByTagName("select");
+const https = require("https");
+
 
 window.addEventListener("load", e => {
   getCurrencies();
@@ -20,4 +22,44 @@ async function getCurrencies() {
     currencySelector[0].appendChild(option.cloneNode(true));
     currencySelector[1].appendChild(option);
   }
+}
+
+
+function convertCurrency(amount, fromCurrency, toCurrency, cb) {
+
+  fromCurrency = encodeURIComponent(fromCurrency);
+  toCurrency = encodeURIComponent(toCurrency);
+  const query = `${fromCurrency}_${toCurrency}`;
+
+  const url = `https://www.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+
+  https.get(url, res => {
+    let body = '';
+
+    res.on('data', chunk => {
+      body += chunk;
+    });
+
+    res.on('end', () => {
+      try {
+        const jsonObj = JSON.parse(body);
+
+        const val = jsonObj[query];
+        if (val) {
+          const total = val * amount;
+          cb(null, Math.round(total * 100) / 100);
+        } else {
+          const err = new Error(`Value not found for ${query}`);
+          console.log(err);
+          cb(err);
+        }
+      } catch (e) {
+        console.log("Parse error: ", e);
+        cb(e);
+      }
+    });
+  }).on('error', e => {
+    console.log("Got an error: ", e);
+    cb(e);
+  });
 }
