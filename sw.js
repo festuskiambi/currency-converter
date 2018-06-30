4e4ee3bfd8a2a92bc2753c0a98bf175c7d602e20
Festus kiambi <kiamfestoh@gmail.com>
2018-06-30 23:56:37 +0300

    add and fetch static assets from the cache

--- sw.js
+++ sw.js
@@ -1,12 +1,28 @@
 /*jshint esversion: 6 */
+const cacheName = 'static-assets-v1';
+const staticAssets = [
+    './',
+    './app.js',
+    './styles.css',
+    './index.html',
+    './images/money.svg',
+    "https://free.currencyconverterapi.com/api/v5/currencies"
+];
 
 self.addEventListener('install', event => {
-
+    event.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(staticAssets))
+    );
 });
 
 self.addEventListener('activate', event => {
 
 });
 self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cacheResponse => {
+            return cacheResponse || fetch(event.request);
+        })
+    );
 
 });
