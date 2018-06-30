31b5aa475ed3730da90e17e707bf0d498d36d376
Festus kiambi <kiamfestoh@gmail.com>
2018-06-30 23:20:19 +0300

    add event listeners to service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+
+self.addEventListener('install', event => {
+
+});
+
+self.addEventListener('activate', event => {
+
+});
+self.addEventListener('fetch', event => {
+
+});
